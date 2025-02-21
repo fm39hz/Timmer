66cdcc0f3fda8d5b18eb423c7c7c01b411fc09da
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-02-21 13:18:29 +0700

    feat: config Seed method

--- Timmer.Api/Database/UserContextExtensions.cs
+++ Timmer.Api/Database/UserContextExtensions.cs
@@ -4,6 +4,7 @@ using Configuration;
 using Constant;
 using Domain.User;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 public static class UserContextExtensions {
@@ -11,25 +12,33 @@ public static class UserContextExtensions {
 		service.AddDbContext<UserContext>(optionsBuilder => {
 			optionsBuilder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
 			optionsBuilder.UseSeeding((context, _) => {
-				var users = context.Set<User>().ToList();
-				var userInfo = new User { Name = userSeed.Name, Email = userSeed.Email };
-				var existedAdmin = users.Count(user =>
-					(user.Role & Roles.Admin) != 0 &&
-					user.Email == userInfo.Email &&
-					user.Name == userInfo.Name);
-				if (existedAdmin > 0) {
-					return;
-				}
-
-				var passwordHasher = new PasswordHasher<User>();
-
-				var admin = new User(userInfo) {
-					Role = Roles.Admin, PasswordHash = passwordHasher.HashPassword(userInfo, userSeed.Password)
-				};
-				context.Set<User>().Add(admin);
+				context.Seed(userSeed);
 				context.SaveChanges();
+			});
+			optionsBuilder.UseAsyncSeeding(async (context, _, cancellationToken) => {
+				context.Seed(userSeed);
+				await context.SaveChangesAsync(cancellationToken);
 			});
 		});
 		return service;
+	}
+
+	private static void Seed(this DbContext context, UserSeed userSeed) {
+		var users = context.Set<User>().ToList();
+		var userInfo = new User { Name = userSeed.Name, Email = userSeed.Email };
+		var existedAdmin = users.Count(user =>
+			(user.Role & Roles.Admin) != 0 &&
+			user.Email == userInfo.Email &&
+			user.Name == userInfo.Name);
+		if (existedAdmin > 0) {
+			return;
+		}
+
+		var passwordHasher = new PasswordHasher<User>();
+
+		var admin = new User(userInfo) {
+			Role = Roles.Admin, PasswordHash = passwordHasher.HashPassword(userInfo, userSeed.Password)
+		};
+		context.Set<User>().Add(admin);
 	}
 }
